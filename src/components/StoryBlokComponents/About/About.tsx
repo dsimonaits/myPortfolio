9c0de17e126ab3c9adb12afc18e1805aea78a6bb
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-07 00:52:27 +0200

    refactoring

--- src/components/StoryBlokComponents/About/About.tsx
+++ src/components/StoryBlokComponents/About/About.tsx
@@ -1,10 +1,28 @@
 import React from "react"
+import { render } from "storyblok-rich-text-react-renderer"
 import MainContainer from "../../Container/Container"
-import { Box, Image, Text, Stack, Heading } from "@chakra-ui/react"
+import {
+  Box,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  HStack,
+  Tag,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  VStack,
+} from "@chakra-ui/react"
 import Section from "../../Section/Section"
+import { AboutStoryblok } from "../../../../component-types-sb"
+import BlurContainer from "../../BlurContainer/BlurContainer"
 
-const About = ({ blok }) => {
-  console.log(blok)
+const About = ({ blok }: AboutStoryblok) => {
+  const aboutMe = blok.aboutMe
+  const moreAbout = blok.moreAbout
   return (
     <Section id="about">
       <MainContainer>
@@ -13,12 +31,24 @@ const About = ({ blok }) => {
           gap={["20px", "40px", "40px"]}
         >
           <Box
+            position="relative"
             flex="1"
             display="flex"
             flexDir="column"
             justifyContent="center"
             alignItems="center"
           >
+            <Box
+              position="absolute"
+              top="10%"
+              left="10%"
+              background="linear-gradient(151deg, rgba(0,98,255,0) 0%, rgba(57,85,242,0.22030237580993517) 21%, rgba(255,41,195,1) 100%)"
+              width="400px"
+              height="400px"
+              borderRadius="50%"
+              filter="blur(70px)"
+              zIndex="0"
+            />
             <Image
               mx="auto"
               src={blok.myImage.filename}
@@ -26,14 +56,33 @@ const About = ({ blok }) => {
               alt="Deniss Simonaits"
               borderRadius="full"
               boxSize={["100px", "150px", "250px"]}
+              zIndex="99"
             />
-            <Heading as="h3" size="md" mt="10px">
+            <Tag size="lg" mt="15px" zIndex="99">
               {blok.title}
-            </Heading>
+            </Tag>
           </Box>
-          <Box flex="1">
-            <Text>{blok.aboutMe}</Text>
-          </Box>
+          <VStack flex="1">
+            <Tag size="lg" p="20px" mb="-20px">
+              About Me
+            </Tag>
+            <BlurContainer>
+              <Box>{render(aboutMe)}</Box>
+              <Accordion allowToggle>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton>
+                      <Box as="span" flex="1" textAlign="left">
+                        Read more...
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel>{render(moreAbout)}</AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </BlurContainer>
+          </VStack>
         </Stack>
       </MainContainer>
     </Section>
