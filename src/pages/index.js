2eb541a66d3bea5139fc58ba83709f9a80058c94
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-07 00:52:48 +0200

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import { StoryblokStory } from "gatsby-source-storyblok"
+import Layout from "../components/Layout/layout"
+import { ChakraProvider } from "@chakra-ui/react"
+import { MainTheme } from "../styles/theme/styles"
+
+const IndexPage = ({ data }) => {
+  if (typeof data.storyblokEntry.content === "string")
+    data.storyblokEntry.content = JSON.parse(data.storyblokEntry.content)
+
+  return (
+    <ChakraProvider theme={MainTheme}>
+      <Layout>
+        <StoryblokStory story={data.storyblokEntry} />
+      </Layout>
+    </ChakraProvider>
+  )
+}
+
+export default IndexPage
+
+export const query = graphql`
+  query HomeQuery {
+    storyblokEntry(full_slug: { eq: "home" }) {
+      content
+      name
+      full_slug
+      uuid
+      id
+      internalId
+    }
+  }
+`
