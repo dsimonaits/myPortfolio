18361435f7a5add95ff2182f0c661b7c45e4d49f
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-07 15:27:08 +0200

    Create Contact.tsx

---
+++ src/components/StoryBlokComponents/Contact/Contact.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+} from "@chakra-ui/accordion"
+import { Box, HStack, Text } from "@chakra-ui/layout"
+
+import { render } from "storyblok-rich-text-react-renderer"
+import { ContactStoryblok } from "../../../../component-types-sb"
+
+const Contact = ({ blok }: ContactStoryblok) => {
+  const contactMe = blok.contactMe
+  const contactMoreInfo = blok.contactMore
+
+  return (
+    <>
+      <HStack>
+        <Box flex="1">{render(contactMe)}</Box>
+        <Accordion allowToggle flex="1">
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box as="span" flex="1" textAlign="left">
+                  Read more...
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel>{render(contactMoreInfo)}</AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </HStack>
+    </>
+  )
+}
+
+export default Contact
