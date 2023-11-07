f069a93d98fdeb78f52a972e2aa2a47c5787e9e3
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-08 00:04:13 +0200

    Update Footer.tsx

--- src/components/StoryBlokComponents/Footer/Footer.tsx
+++ src/components/StoryBlokComponents/Footer/Footer.tsx
@@ -5,6 +5,7 @@ import MainContainer from "../../Container/Container"
 import { FooterStoryblok } from "../../../../component-types-sb"
 import BlurContainer from "../../BlurContainer/BlurContainer"
 import { Tag } from "@chakra-ui/tag"
+import { Box } from "@chakra-ui/react"
 
 const Footer = ({ blok }: FooterStoryblok) => {
   return (
@@ -13,7 +14,18 @@ const Footer = ({ blok }: FooterStoryblok) => {
         <MainContainer
           style={{ display: "flex", flexDirection: "column", gap: "10px" }}
         >
-          <Tag size="lg" p="20px" mb="-20px" mx="auto">
+          <Box
+            position="absolute"
+            top="-200%"
+            left="0%"
+            background="linear-gradient(151deg, rgba(0,98,255,0) 60%, rgba(57,85,242,0.22030237580993517) 81%), rgba(255,41,195,0.3) 80%"
+            width={["300px", "500px", "700px"]}
+            height={["300px", "500px", "700px"]}
+            borderRadius="50%"
+            filter="blur(70px)"
+            zIndex="0"
+          />
+          <Tag size="lg" p="20px" mb="-20px" mx="auto" zIndex="99">
             Contact Me
           </Tag>
           <BlurContainer>
