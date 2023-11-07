d7d0ab28d7117a78935045430cf1a8077172d928
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-07 18:03:49 +0200

    Create SocialLink.tsx

---
+++ src/components/StoryBlokComponents/SocialLink/SocialLink.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { SocialLinkStoryblok } from "../../../../component-types-sb"
+import { Image, Link } from "@chakra-ui/react"
+
+const SocialLink = ({ blok }: SocialLinkStoryblok) => {
+  console.log(blok)
+  return (
+    <Link key={blok._uid} href={blok.url.url} target="_blank">
+      <Image src={blok.icon.filename} alt={blok.url.url} width="40px" />
+    </Link>
+  )
+}
+
+export default SocialLink
