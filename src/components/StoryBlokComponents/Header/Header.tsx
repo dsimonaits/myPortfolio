7c89b047af7d6d54552e3d77bc86bb69729db8da
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-13 22:20:35 +0200

    Create Header.tsx

---
+++ src/components/StoryBlokComponents/Header/Header.tsx
@@ -0,0 +1,79 @@
+import React from "react"
+// import { useState } from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import { INode } from "../../../types/types"
+import { MenuLinkStoryblok } from "../../../../component-types-sb"
+import { Box, HStack } from "@chakra-ui/layout"
+import MainContainer from "../../Container/Container"
+import useScrollPosition from "../../../hooks/useScrollPosition"
+
+const Header = () => {
+  const { header } = useStaticQuery(graphql`
+    {
+      header: allStoryblokEntry(filter: { field_component: { eq: "header" } }) {
+        edges {
+          node {
+            name
+            uuid
+            content
+          }
+        }
+      }
+    }
+  `)
+
+  // const [openMenu, setOpenMenu] = useState(false)
+
+  const thisHeader = header.edges.filter(({ node }: INode) => node.uuid)
+  const headerContent = thisHeader.length
+    ? JSON.parse(thisHeader[0].node.content)
+    : {}
+
+  const menu = headerContent.HeaderMenu.map((menu: MenuLinkStoryblok[]) => menu)
+
+  const Logo = headerContent.Logo[0]
+
+  console.log(Logo)
+
+  const Nav = () =>
+    menu.map((item: MenuLinkStoryblok) => (
+      <Box as="li" key={item._uid}>
+        <Link to={`${item?.link?.url}`}>{item.name}</Link>
+      </Box>
+    ))
+
+  return (
+    <Box
+      as="section"
+      position="fixed"
+      py="30px"
+      bg="black"
+      top={0}
+      left={0}
+      right={0}
+      zIndex="100"
+      transform={useScrollPosition() ? "translateY(-200px)" : "translateY(0)"}
+      transitionProperty="transform"
+      transitionDuration=".3s"
+      transitionTimingFunction="ease-in-out"
+      fontSize="xl"
+    >
+      <MainContainer>
+        <HStack
+          justify="space-between"
+          align="center"
+          bg="black"
+          zIndex="100"
+          px="40px"
+        >
+          <Box>{Logo.logoName}</Box>
+          <HStack as="ul" gap="30px">
+            <Nav />
+          </HStack>
+        </HStack>
+      </MainContainer>
+    </Box>
+  )
+}
+
+export default Header
