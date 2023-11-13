e0e4d050bec0fefe56e3f6b3ebbd883d6b8c96f7
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-13 22:20:18 +0200

    Create types.tsx

---
+++ src/types/types.tsx
@@ -0,0 +1,14 @@
+export interface nodeContent {
+  name: string
+  uuid: string
+  content: string
+}
+
+export interface INode {
+  node: nodeContent
+}
+
+export interface Menu {
+  link: "string"
+  name: "string"
+}
