06109d162c975bb9805f294b42822eba4087b4cc
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-13 22:20:29 +0200

    Update SocialLink.tsx

--- src/components/StoryBlokComponents/SocialLink/SocialLink.tsx
+++ src/components/StoryBlokComponents/SocialLink/SocialLink.tsx
@@ -3,7 +3,6 @@ import { SocialLinkStoryblok } from "../../../../component-types-sb"
 import { Image, Link } from "@chakra-ui/react"
 
 const SocialLink = ({ blok }: SocialLinkStoryblok) => {
-  console.log(blok)
   return (
     <Link key={blok._uid} href={blok.url.url} target="_blank">
       <Image src={blok.icon.filename} alt={blok.url.url} width="40px" />
