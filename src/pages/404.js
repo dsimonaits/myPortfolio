dd9d058642feab600437146ac7aea15ac69bfb27
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-14 14:24:13 +0200

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,11 +2,20 @@
 import * as React from "react"
 import Layout from "../components/Layout/layout"
 import Seo from "../components/seo"
+import Section from "../components/Section/Section"
+import MainContainer from "../components/Container/Container"
+import { VStack } from "@chakra-ui/react"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Section>
+      <MainContainer>
+        <VStack minH="300px" justifyContent="center" alignItems="center">
+          <h1>404: Not Found</h1>
+          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </VStack>
+      </MainContainer>
+    </Section>
   </Layout>
 )
 
