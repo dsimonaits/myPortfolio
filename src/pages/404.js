6e5c420f30397c8402776977412f61d74eca84b5
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-14 14:35:48 +0200

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,15 +4,26 @@ import Layout from "../components/Layout/layout"
 import Seo from "../components/seo"
 import Section from "../components/Section/Section"
 import MainContainer from "../components/Container/Container"
-import { VStack } from "@chakra-ui/react"
+import { navigate } from "gatsby"
+import { Heading, Text, VStack, Button } from "@chakra-ui/react"
 
 const NotFoundPage = () => (
   <Layout>
     <Section>
-      <MainContainer>
+      <MainContainer style={{ zIndex: "100" }}>
         <VStack minH="300px" justifyContent="center" alignItems="center">
-          <h1>404: Not Found</h1>
-          <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+          <Heading as="h3">404: page not found</Heading>
+          <Text fontSize="lg">
+            You just hit a route that doesn&#39;t exist... the sadness.
+          </Text>
+
+          <Button
+            _hover={{ bg: "secondary.600", color: "main.100" }}
+            transition="all 250ms cubic-bezier(0.4, 0, 0.2, 1)"
+            onClick={() => navigate(-1)}
+          >
+            Go back
+          </Button>
         </VStack>
       </MainContainer>
     </Section>
