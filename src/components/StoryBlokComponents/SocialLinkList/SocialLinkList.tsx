28c3c12ea9cc83bbbca04dbe00c4efbf72366ec5
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-14 14:11:46 +0200

    Update SocialLinkList.tsx

--- src/components/StoryBlokComponents/SocialLinkList/SocialLinkList.tsx
+++ src/components/StoryBlokComponents/SocialLinkList/SocialLinkList.tsx
@@ -1,10 +1,7 @@
 import React from "react"
 import { StoryblokComponent } from "gatsby-source-storyblok"
 import { List, ListItem } from "@chakra-ui/layout"
-import {
-  SocialLinkListStoryblok,
-  SocialLinkStoryblok,
-} from "../../../../component-types-sb"
+import { SocialLinkListStoryblok } from "../../../../component-types-sb"
 
 interface SocialLinkListProps {
   blok: SocialLinkListStoryblok
@@ -13,7 +10,7 @@ interface SocialLinkListProps {
 const SocialLinkList: React.FC<SocialLinkListProps> = ({ blok }) => {
   return (
     <List display="flex" flexDir="row" gap="10px" justifyContent="center">
-      {blok.socialLinks.map((blok: SocialLinkStoryblok) => (
+      {blok?.socialLinks?.map(blok => (
         <ListItem key={blok._uid}>
           <StoryblokComponent blok={blok} />
         </ListItem>
