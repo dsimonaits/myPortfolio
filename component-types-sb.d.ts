ceb48a01c5d52b72faf60d4253ec8e1c76d54297
Deniss Simonaits <dsimonaits@gmail.com>
2023-11-16 13:40:15 +0200

    config update

--- component-types-sb.d.ts
+++ component-types-sb.d.ts
@@ -1,116 +1,112 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { StoryblokStory } from "storyblok-generate-ts"
+import {StoryblokStory} from 'storyblok-generate-ts'
 
 export interface AssetStoryblok {
-  alt?: string
-  copyright?: string
-  id: number
-  filename: string
-  name: string
-  title?: string
-  focus?: string
-  [k: string]: any
+  alt?: string;
+  copyright?: string;
+  id: number;
+  filename: string;
+  name: string;
+  title?: string;
+  focus?: string;
+  [k: string]: any;
 }
 
 export interface RichtextStoryblok {
-  type: string
-  content?: RichtextStoryblok[]
-  marks?: RichtextStoryblok[]
-  attrs?: any
-  text?: string
-  [k: string]: any
+  type: string;
+  content?: RichtextStoryblok[];
+  marks?: RichtextStoryblok[];
+  attrs?: any;
+  text?: string;
+  [k: string]: any;
 }
 
 export interface AboutStoryblok {
-  myImage?: AssetStoryblok
-  title?: string
-  aboutMe?: RichtextStoryblok
-  moreAbout?: RichtextStoryblok
-  _uid: string
-  component: "about"
-  [k: string]: any
+  myImage?: AssetStoryblok;
+  title?: string;
+  aboutMe?: RichtextStoryblok;
+  moreAbout?: RichtextStoryblok;
+  _uid: string;
+  component: "about";
+  [k: string]: any;
 }
 
 export interface AllArticlesStoryblok {
-  title?: string
-  _uid: string
-  component: "all-articles"
-  [k: string]: any
+  title?: string;
+  _uid: string;
+  component: "all-articles";
+  [k: string]: any;
 }
 
 export type MultilinkStoryblok =
   | {
-      id?: string
-      cached_url?: string
-      anchor?: string
-      linktype?: "story"
+      id?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "story";
       story?: {
-        name: string
-        created_at?: string
-        published_at?: string
-        id: number
-        uuid: string
+        name: string;
+        created_at?: string;
+        published_at?: string;
+        id: number;
+        uuid: string;
         content?: {
-          [k: string]: any
-        }
-        slug: string
-        full_slug: string
-        sort_by_date?: null | string
-        position?: number
-        tag_list?: string[]
-        is_startpage?: boolean
-        parent_id?: null | number
+          [k: string]: any;
+        };
+        slug: string;
+        full_slug: string;
+        sort_by_date?: null | string;
+        position?: number;
+        tag_list?: string[];
+        is_startpage?: boolean;
+        parent_id?: null | number;
         meta_data?: null | {
-          [k: string]: any
-        }
-        group_id?: string
-        first_published_at?: string
-        release_id?: null | number
-        lang?: string
-        path?: null | string
-        alternates?: any[]
-        default_full_slug?: null | string
-        translated_slugs?: null | any[]
-        [k: string]: any
-      }
-      [k: string]: any
+          [k: string]: any;
+        };
+        group_id?: string;
+        first_published_at?: string;
+        release_id?: null | number;
+        lang?: string;
+        path?: null | string;
+        alternates?: any[];
+        default_full_slug?: null | string;
+        translated_slugs?: null | any[];
+        [k: string]: any;
+      };
+      [k: string]: any;
     }
   | {
-      url?: string
-      cached_url?: string
-      anchor?: string
-      linktype?: "asset" | "url"
-      [k: string]: any
+      url?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "asset" | "url";
+      [k: string]: any;
     }
   | {
-      email?: string
-      linktype?: "email"
-      [k: string]: any
-    }
+      email?: string;
+      linktype?: "email";
+      [k: string]: any;
+    };
 
 export interface ArticleStoryblok {
-  image?: AssetStoryblok
-  title?: string
-  subtitle?: string
-  teaser?: string
-  content?: RichtextStoryblok
-  link?: Exclude<
-    MultilinkStoryblok,
-    { linktype?: "email" } | { linktype?: "asset" }
-  >
-  _uid: string
-  component: "article"
-  [k: string]: any
+  image?: AssetStoryblok;
+  title?: string;
+  subtitle?: string;
+  teaser?: string;
+  content?: RichtextStoryblok;
+  link?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  _uid: string;
+  component: "article";
+  [k: string]: any;
 }
 
 export interface ContactStoryblok {
-  heading?: string
-  readMore?: string
-  contactMe?: RichtextStoryblok
-  contactMore?: RichtextStoryblok
-  _uid: string
-  component: "contact"
-  [k: string]: any
+  heading?: string;
+  readMore?: string;
+  contactMe?: RichtextStoryblok;
+  contactMore?: RichtextStoryblok;
+  _uid: string;
+  component: "contact";
+  [k: string]: any;
 }
 
 export interface FooterStoryblok {
@@ -123,16 +119,17 @@ export interface FooterStoryblok {
     | HeaderStoryblok
     | LogoStoryblok
     | MenuLinkStoryblok
+    | MySkillsStoryblok
     | PageStoryblok
     | PopularArticlesStoryblok
     | SkillStoryblok
     | SkillsListStoryblok
     | SocialLinkStoryblok
     | SocialLinkListStoryblok
-  )[]
-  _uid: string
-  component: "footer"
-  [k: string]: any
+  )[];
+  _uid: string;
+  component: "footer";
+  [k: string]: any;
 }
 
 export interface HeaderStoryblok {
@@ -145,13 +142,14 @@ export interface HeaderStoryblok {
     | HeaderStoryblok
     | LogoStoryblok
     | MenuLinkStoryblok
+    | MySkillsStoryblok
     | PageStoryblok
     | PopularArticlesStoryblok
     | SkillStoryblok
     | SkillsListStoryblok
     | SocialLinkStoryblok
     | SocialLinkListStoryblok
-  )[]
+  )[];
   HeaderMenu?: (
     | AboutStoryblok
     | AllArticlesStoryblok
@@ -161,35 +159,56 @@ export interface HeaderStoryblok {
     | HeaderStoryblok
     | LogoStoryblok
     | MenuLinkStoryblok
+    | MySkillsStoryblok
     | PageStoryblok
     | PopularArticlesStoryblok
     | SkillStoryblok
     | SkillsListStoryblok
     | SocialLinkStoryblok
     | SocialLinkListStoryblok
-  )[]
-  _uid: string
-  component: "header"
-  [k: string]: any
+  )[];
+  _uid: string;
+  component: "header";
+  [k: string]: any;
 }
 
 export interface LogoStoryblok {
-  logoImg?: AssetStoryblok
-  logoName?: string
-  _uid: string
-  component: "logo"
-  [k: string]: any
+  logoImg?: AssetStoryblok;
+  logoName?: string;
+  _uid: string;
+  component: "logo";
+  [k: string]: any;
 }
 
 export interface MenuLinkStoryblok {
-  link?: Exclude<
-    MultilinkStoryblok,
-    { linktype?: "email" } | { linktype?: "asset" }
-  >
-  name?: string
-  _uid: string
-  component: "menuLink"
-  [k: string]: any
+  link?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  name?: string;
+  _uid: string;
+  component: "menuLink";
+  [k: string]: any;
+}
+
+export interface MySkillsStoryblok {
+  mySkills?: (
+    | AboutStoryblok
+    | AllArticlesStoryblok
+    | ArticleStoryblok
+    | ContactStoryblok
+    | FooterStoryblok
+    | HeaderStoryblok
+    | LogoStoryblok
+    | MenuLinkStoryblok
+    | MySkillsStoryblok
+    | PageStoryblok
+    | PopularArticlesStoryblok
+    | SkillStoryblok
+    | SkillsListStoryblok
+    | SocialLinkStoryblok
+    | SocialLinkListStoryblok
+  )[];
+  _uid: string;
+  component: "mySkills";
+  [k: string]: any;
 }
 
 export interface PageStoryblok {
@@ -202,32 +221,33 @@ export interface PageStoryblok {
     | HeaderStoryblok
     | LogoStoryblok
     | MenuLinkStoryblok
+    | MySkillsStoryblok
     | PageStoryblok
     | PopularArticlesStoryblok
     | SkillStoryblok
     | SkillsListStoryblok
     | SocialLinkStoryblok
     | SocialLinkListStoryblok
-  )[]
-  _uid: string
-  component: "page"
-  uuid?: string
-  [k: string]: any
+  )[];
+  _uid: string;
+  component: "page";
+  uuid?: string;
+  [k: string]: any;
 }
 
 export interface PopularArticlesStoryblok {
-  articles?: (StoryblokStory<ArticleStoryblok> | string)[]
-  _uid: string
-  component: "popular-articles"
-  [k: string]: any
+  articles?: (StoryblokStory<ArticleStoryblok> | string)[];
+  _uid: string;
+  component: "popular-articles";
+  [k: string]: any;
 }
 
 export interface SkillStoryblok {
-  name?: string
-  icon?: AssetStoryblok
-  _uid: string
-  component: "skill"
-  [k: string]: any
+  name?: string;
+  icon?: AssetStoryblok;
+  _uid: string;
+  component: "skill";
+  [k: string]: any;
 }
 
 export interface SkillsListStoryblok {
@@ -240,29 +260,27 @@ export interface SkillsListStoryblok {
     | HeaderStoryblok
     | LogoStoryblok
     | MenuLinkStoryblok
+    | MySkillsStoryblok
     | PageStoryblok
     | PopularArticlesStoryblok
     | SkillStoryblok
     | SkillsListStoryblok
     | SocialLinkStoryblok
     | SocialLinkListStoryblok
-  )[]
-  _uid: string
-  component: "skills-list"
-  [k: string]: any
+  )[];
+  _uid: string;
+  component: "skills-list";
+  [k: string]: any;
 }
 
 export interface SocialLinkStoryblok {
-  icon?: AssetStoryblok
-  name?: string
-  type?: string
-  url?: Exclude<
-    MultilinkStoryblok,
-    { linktype?: "email" } | { linktype?: "asset" }
-  >
-  _uid: string
-  component: "socialLink"
-  [k: string]: any
+  icon?: AssetStoryblok;
+  name?: string;
+  type?: string;
+  url?: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  _uid: string;
+  component: "socialLink";
+  [k: string]: any;
 }
 
 export interface SocialLinkListStoryblok {
@@ -275,14 +293,15 @@ export interface SocialLinkListStoryblok {
     | HeaderStoryblok
     | LogoStoryblok
     | MenuLinkStoryblok
+    | MySkillsStoryblok
     | PageStoryblok
     | PopularArticlesStoryblok
     | SkillStoryblok
     | SkillsListStoryblok
     | SocialLinkStoryblok
     | SocialLinkListStoryblok
-  )[]
-  _uid: string
-  component: "socialLinkList"
-  [k: string]: any
+  )[];
+  _uid: string;
+  component: "socialLinkList";
+  [k: string]: any;
 }
